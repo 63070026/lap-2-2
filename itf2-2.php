13638b817dc78ef891b699823ba395f314fc9880
63070026 <73680622+63070026@users.noreply.github.com>
2020-11-06 16:49:49 +0700

    Update itf2-2.php

--- itf2-2.php
+++ itf2-2.php
@@ -1,1 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-header bg-danger text-white">
+                        <img src="office1.jpg" class="img-fluid rounded" />
+                    </div>
+                    <div class="card-body">
+                        <p>This is a computer.</p>
+                    </div>
+                    <div class="card-footer">
+                        By 026
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-header bg-danger text-white">
+                        <img src="work9.jpg" class="img-fluid rounded" />
+                    </div>
+                    <div class="card-body">
+                        <p>I'm working on my computer .</p>
+                    </div>
+                    <div class="card-footer">
+                        By 026
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-header bg-danger text-white">
+                        <img src="staff4.jpg" class="img-fluid rounded" />
+                    </div>
+                    <div class="card-body">
+                        <p>She is my staff.</p>
+                    </div>
+                    <div class="card-footer">
+                        By 026
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div> 
+</body>
+
+</html>
