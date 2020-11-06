8d0e07a014ebe5a350b57f03a411254bb7b95d0a
63070026 <73680622+63070026@users.noreply.github.com>
2020-11-06 17:38:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,39 +16,46 @@
             <div class="col-sm-3">
                 <div class="card">
                     <div class="card-header bg-light text-white">
-                        <img src="office1.jpg" class="img-fluid rounded" />
+                        <img src="siderman.jpg" class="img-fluid rounded" />
                     </div>
                     <div class="card-body">
-                        <p>This is my computer that i am working on.</p>
+                        <p>Spider-Man: Far From Home is a 2019 American superhero film based on the Marvel Comics character Spider-Man, 
+                            co-produced by Columbia Pictures and Marvel Studios, and distributed by Sony Pictures Releasing. 
+                            It is the sequel to Spider-Man: Homecoming (2017)</p>
                     </div>
                     <div class="card-footer">
-                        By 026
+                        By https://en.wikipedia.org/wiki/Spider-Man:_Far_From_Home
                     </div>
                 </div>
             </div>
             <div class="col-sm-3">
                 <div class="card">
                     <div class="card-header bg-light text-white">
-                        <img src="work9.jpg" class="img-fluid rounded" />
+                        <img src="demon slayer.jpg" class="img-fluid rounded" />
                     </div>
                     <div class="card-body">
-                        <p>I'm working on my computer .</p>
+                        <p>In Taisho-era Japan, Tanjiro Kamado is a kindhearted boy who makes a living selling charcoal. 
+                            However, his peaceful life is shattered when a Demon slaughters his entire family. 
+                            His little sister Nezuko is the only survivor, but she has been transformed into a Demon herself! 
+                            Tanjiro sets out on a dangerous journey to find a way to return his sister to normal and destroy the Demon who ruined his life. .</p>
                     </div>
                     <div class="card-footer">
-                        By 026
+                        By https://kimetsu-no-yaiba.fandom.com/wiki/Kimetsu_no_Yaiba_Wiki
                     </div>
                 </div>
             </div>
             <div class="col-sm-3">
                 <div class="card">
                     <div class="card-header bg-light text-white">
-                        <img src="staff4.jpg" class="img-fluid rounded" />
+                        <img src="valorant.jpg" class="img-fluid rounded" />
                     </div>
                     <div class="card-body">
-                        <p>She is the one who helps me work .</p>
+                        <p>  is a free-to-play multiplayer tactical first-person shooter developed and published by Riot Games, 
+                            for Microsoft Windows. First teased under the codename Project A in October 2019, 
+                            the game began a closed beta period with limited access on April 7, 2020, followed by official release on June 2, 2020..</p>
                     </div>
                     <div class="card-footer">
-                        By 026
+                        By https://en.wikipedia.org/wiki/Valorant
                     </div>
                 </div>
             </div>
